c2733ad4c9149f0c2471bf4979d0e4d6bdac23c0
fodaysahid <fodaysnkamara@gmail.com>
2020-01-02 10:51:54 +0000

    Make changes

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=1C_0WiXRfOHcxeg3uXRYh17vNcv6eGQhx'
+export_file_name = 'FodayCassavaUpdated.pth'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['cbsd', 'cgm', 'cmd', 'cbb', 'healthy']
 path = Path(__file__).parent
 
 app = Starlette()
