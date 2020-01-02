fb700383bcfa9bf4f0cf8eed274dc3592bf9b1c4
fodaysahid <fodaysnkamara@gmail.com>
2020-01-02 11:03:55 +0000

    Change App Export Url

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1C_0WiXRfOHcxeg3uXRYh17vNcv6eGQhx'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1C_0WiXRfOHcxeg3uXRYh17vNcv6eGQhx?alt=media&key=AIzaSyBfTuUWZVBa1O5ZF7Q0vpkhaGYY67T4weY'
 export_file_name = 'FodayCassavaUpdated.pth'
 
 classes = ['cbsd', 'cgm', 'cmd', 'cbb', 'healthy']
